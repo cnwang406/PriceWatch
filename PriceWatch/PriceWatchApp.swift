39e1b13d9883e65d00460a5a6df990ae62881a65
cnwang <cnwang406@gmail.com>
2023-05-16 17:24:56 +0800

    Initial Commit

---
+++ PriceWatch/PriceWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceWatchApp.swift
+//  PriceWatch
+//
+//  Created by Chun nan Wang on 2023/5/16.
+//
+
+import SwiftUI
+
+@main
+struct PriceWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
